a7c21beb548ea5c09cb3beee2c66624e7131c21c
Luis <Luis@LAPTOP-IKGKEAKR>
2024-07-10 20:23:34 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+
+namespace CursoINT
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        { 
+            Queue miFila = new Queue();
+            //primera persona en llegar luis
+            miFila.Enqueue("Luis");
+            miFila.Enqueue("Pedrio");
+            miFila.Enqueue("Laura");
+
+            Stack miPila = new Stack();
+
+            miPila.Push("Luis");
+            miPila.Push("Laura");
+            miPila.Push("David");
+
+            Console.WriteLine(miPila.Count);
+
+            miPila.Clear();
+
+            Console.WriteLine(miPila.Count);
+
+            int tamannioPila = miPila.Count;
+
+            for (int i = 0; i < tamannioPila; i++)
+            {
+                Console.WriteLine("Los valores de la pila son" + miPila.Pop());
+            }
+
+            Queue filaClon = (Queue)miFila.Clone();
+
+
+            Console.WriteLine(miFila.Count);
+            for (int i = 0; i < 3; i ++)
+            {
+                Console.WriteLine("La señora atendera a:" + miFila.Dequeue());
+            }
+
+            Console.WriteLine(miFila.Count);
+
+            Console.WriteLine("Los valores de filaclon son: " + filaClon.Count);
+
+            filaClon.Clear();
+
+            Console.WriteLine("Los valores de filaclon son: " + filaClon.Count);
+        }
+    }
+}
